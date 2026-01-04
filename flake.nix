52de52988958fcb9aa87eb6fd8a1181b40c8bf2a
clippyricer <bannansmooth68@outlook.com>
2026-01-04 13:37:35 -0500

    idk

--- flake.nix
+++ flake.nix
@@ -5,7 +5,7 @@
     nixpkgs.url = "github:nixos/nixpkgs?ref=nixos-unstable";
     home-manager.url = "github:nix-community/home-manager/master";
     home-manager.inputs.nixpkgs.follows = "nixpkgs";
-    hyprland.url = "github:hyprwm/Hyprland";
+    inputs.hyprland.url = "github:hyprwm/Hyprland";
 
   };
 
